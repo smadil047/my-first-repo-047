f5454f45a32bbf916b1ccd53b2df61a94e150caf
smadil047 <smadil047@gmail.com>
2025-01-24 01:21:11 +0530

    Add files via upload

---
+++ code.cpp
@@ -0,0 +1,13 @@
+// C++ program to add two number using
+// addition operator
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a = 11, b = 9;
+
+    // Adding the two numbers and printing
+    // their sum
+    cout << a + b;
+    return 0;
+}
